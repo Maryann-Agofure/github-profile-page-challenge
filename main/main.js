666be24233d6b5b39513a02398656957488f4053
Maryann Agofure <maryannagofure@gmail.com>
2021-05-25 21:59:40 +0100

    query is finally querying

--- main/main.js
+++ main/main.js
@@ -1,8 +1,95 @@
 let user = document.querySelector('.userSearch');
 const userSearchButton = document.getElementById('searchButton')
+let myData = document.querySelector('.getMyData')
 
-let query ={
-  'query': `($name: String= ${user.value}, $count:Int= 12, $withFork: Boolean=true, $withStar: Boolean=true) {
+
+
+//get inputed search query 
+
+
+function showUserInfo(){
+userSearchButton.addEventListener('click', getRepoData)
+}
+showUserInfo();
+
+
+async function getRepoData() {
+let content = JSON.stringify({
+  'query':
+  `
+    query getRepositories($name: String!, $count:Int= 12, $withFork: Boolean=true, $withStar: Boolean=true) {
+    repositoryOwner(login: $name){
+      avatarUrl
+      id
+      login
+      
+      repositories(first:$count ){
+        nodes{
+          name
+          description
+          
+          primaryLanguage{
+            color
+            name
+          }
+          stargazerCount@include(if: $withStar)
+          forkCount@include(if: $withFork)
+          updatedAt
+          
+        }
+      }
+    
+    }
+  }
+
+`,
+variables: `{
+  "name": "${user.value}"
+}`
+})
+
+
+  let response = await fetch(' https://api.github.com/graphql',{
+    method: 'POST', // or 'PUT'
+    body: content,
+    credentials:"same-origin",
+    mode: "cors",
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': content.length,
+      Authorization: "token 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"
+    }}) ;
+
+ const repoReport = await response.json()
+ myData.innerHTML = (JSON.stringify(repoReport)
+);
+
+let profileInfo = repoReport.data.repositoryOwner
+
+console.log(profileInfo)
+
+}
+
+getRepoData();
+
+
+
+// step by step 
+//step 1: import js module to enable us make GraphQL queries in javascript
+//import { graphql } from "https://cdn.skypack.dev/@octokit/graphql";
+
+//step 2: Request by providing the query we want ccalled, our Github access token and an authorization header with our token
+
+
+
+
+ 
+
+// get access token and create header with tokentoken 
+
+/*query:
+   `
+    ($name: String= ${user.value}, $count:Int= 12, $withFork: Boolean=true, $withStar: Boolean=true) {
     repositoryOwner(login: $name,){
       avatarUrl
       id
@@ -25,100 +112,35 @@ let query ={
       }
     }
     }
-  
-`
   }
-
-//get inputed search query 
-
-
-function showUserInfo(){
-userSearchButton.addEventListener('click', console.log('ann'))
-}
-showUserInfo
-
-function loadUser(){
-  let data =  {}
-let body = JSON.stringify(query);
-
-fetch('https://graphql.github.com/graphql/proxy', {
-  method: 'POST', // or 'PUT'
-  credentials:"same-origin",
-  mode: "cors",
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: body,
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-}
-loadUser()
-
-
-
-
-// step by step 
-//step 1: import js module to enable us make GraphQL queries in javascript
-//import { graphql } from "https://cdn.skypack.dev/@octokit/graphql";
-
-//step 2: Request by providing the query we want ccalled, our Github access token and an authorization header with our token
-
-
-
-
- 
-
-// get access token and create header with tokentoken 
-
-/*const token = "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"
-const auth = {
-    headers: {
-        authorization: "token " + token
+`
     }
 }
 
-//step 3: create function to make request
-const myRequest = async(q, a) => await graphql(q, a)
-
-console.log(myRequest(query, auth))*/
-
-//let body = JSON.stringify(query);
-/*{
-method: 'POST',
- mode: "cors",
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: body
-}*/
-
-
-
-
-  /*
-fetch("https://graphql.github.com/graphql/proxy", data = {},
-{
-  
-  method: 'POST',
-   mode: "cors",
-   credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-      
-
-    },
-    body: JSON.stringify(data)
-  }
-)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
 */
+
+/*getRepositories($name: String= "Maryann-Agofure", $count:Int= 12, $withFork: Boolean=true, $withStar: Boolean=true) {
+  repositoryOwner(login: $name,){
+    avatarUrl
+    id
+    login
+    
+    repositories(first:$count ){
+      nodes{
+        name
+        description
+        
+        primaryLanguage{
+          color
+          name
+        }
+        stargazerCount@include(if: $withStar)
+        forkCount@include(if: $withFork)
+        updatedAt
+        
+      }
+    }
   
+  }
+}
+*/
